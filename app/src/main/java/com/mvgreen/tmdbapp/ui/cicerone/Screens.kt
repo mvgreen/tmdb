fb45d3a9d947d1a8377825ad6968910aa3a56e0f
Vladimir Mikhailov <v.mikhailov@redmadrobot.com>
2020-04-09 17:47:24 +0400

    [Profile] Добавил иерархию фрагментов.

--- app/src/main/java/com/mvgreen/tmdbapp/ui/cicerone/Screens.kt
+++ app/src/main/java/com/mvgreen/tmdbapp/ui/cicerone/Screens.kt
@@ -1,9 +1,11 @@
 package com.mvgreen.tmdbapp.ui.cicerone
 
+import androidx.fragment.app.Fragment
 import com.mvgreen.tmdbapp.ui.auth.fragment.AuthFragment
 import com.mvgreen.tmdbapp.ui.favorites.fragment.FavoritesBranchFragment
 import com.mvgreen.tmdbapp.ui.films.fragment.FilmsBranchFragment
 import com.mvgreen.tmdbapp.ui.profile.fragment.ProfileBranchFragment
+import com.mvgreen.tmdbapp.ui.profile.fragment.ProfileFragment
 import com.mvgreen.tmdbapp.ui.rootscreen.fragment.RootFragment
 import ru.terrakok.cicerone.android.support.SupportAppScreen
 
@@ -26,4 +28,8 @@ object FavoritesBranchScreen : SupportAppScreen() {
 
 object ProfileBranchScreen : SupportAppScreen() {
     override fun getFragment() = ProfileBranchFragment()
+}
+
+object ProfileScreen : SupportAppScreen() {
+    override fun getFragment() = ProfileFragment()
 }
---
+++ app/src/main/java/com/mvgreen/tmdbapp/ui/profile/fragment/ProfileFragment.kt
@@ -0,0 +1,8 @@
+package com.mvgreen.tmdbapp.ui.profile.fragment
+
+import com.mvgreen.tmdbapp.R
+import com.mvgreen.tmdbapp.ui.base.fragment.BaseFragment
+
+class ProfileFragment: BaseFragment(R.layout.fragment_profile) {
+
+}
