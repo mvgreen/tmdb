96ec7ae1d58fc9e160bdf64be7470b1a27999406
Vladimir Mikhailov <v.mikhailov@redmadrobot.com>
2020-04-09 17:32:05 +0400

    [Profile] Закончил интерфейс

--- app/src/main/java/com/mvgreen/tmdbapp/ui/profile/fragment/ProfileBranchFragment.kt
+++ app/src/main/java/com/mvgreen/tmdbapp/ui/profile/fragment/ProfileBranchFragment.kt
@@ -3,4 +3,13 @@ package com.mvgreen.tmdbapp.ui.profile.fragment
 import com.mvgreen.tmdbapp.R
 import com.mvgreen.tmdbapp.ui.base.fragment.BaseFragment
 
-class ProfileBranchFragment : BaseFragment(R.layout.branch_stub_profile)
+class ProfileBranchFragment : BaseFragment(R.layout.branch_profile) {
+
+
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+
+}
--- domain/src/main/java/com/mvgreen/domain/bean/ImageLoader.kt
+++ domain/src/main/java/com/mvgreen/domain/bean/ImageLoader.kt
@@ -2,11 +2,16 @@ package com.mvgreen.domain.bean
 
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 class ImageLoader(private val url: String) {
 
     fun loadImage (view: ImageView) {
-        Glide.with(view).load(url).into(view)
+        Glide
+            .with(view)
+            .load(url)
+            .apply(RequestOptions.circleCropTransform())
+            .into(view)
     }
 
 }
